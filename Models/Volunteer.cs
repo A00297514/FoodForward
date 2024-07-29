863d7672408b07791bf4c1cdcd610ff2f02abc2d
Pratham <A00303235@mycambrian.ca>
2024-07-29 18:07:31 -0400

    Created Volunteer Model and coded it

---
+++ Models/Volunteer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FoodForwardApp.Models
+{
+    /// <summary>
+    /// The Volunteer class represents the details of a volunteer
+    /// registering for the FoodForwardApp. It includes fields for capturing
+    /// the volunteer's name, contact information, address, and estimated time.
+    /// </summary>
+    public class Volunteer
+    {
+        // Stores the Volunteer's Name
+        public string Name { get; set; }
+
+        // Stores the Volunteer's Number
+        public string Number { get; set; }
+
+        // Stores the Volunteer's Address
+        public string Address { get; set; }
+
+        // Stores the Estimated Time
+        public string EstimatedTime { get; set; }
+    }
+}
